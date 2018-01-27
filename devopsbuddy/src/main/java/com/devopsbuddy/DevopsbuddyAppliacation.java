9c93364d6eb183915bcdb7d470a75f688ce9e1e3
Josip Punis <josippunis@gmail.com>
2018-01-27 12:20:28 +0100

    arch

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/DevopsbuddyAppliacation.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DevopsbuddyAppliacation {
+    public static void main(String args[]){
+        SpringApplication.run(DevopsbuddyAppliacation.class, args);
+    }
+}
